3c2e6313e2caa6e26c15a8abb72b68d39e980b3a
VikasGanesh <vikasyrg@gmail.com>
2018-05-29 22:25:49 +0530

    user session

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,flash
+from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user, current_user
+
 app=Flask(__name__)
+
+class User(UserMixin):
+    def __init__(self,name,password):
+        self.name = name
+        self.password = password
+    def __repr__(self):
+        return "%s/%s" % (self.name, self.password)
 
 
 @app.route('/')
@@ -10,7 +19,18 @@ def Home():
 def Login():
     username=request.form['username']
     password=request.form['password']
-    
+    user=""
+    if username=="admim":
+        if password=="admin":
+            user=User(str(username),str(password))
+            login_user(user)
+        else:
+            flash('msg',"False")
+            return render_template('login.html')
+    else:
+        flash('msg',"False")
+        return render_template('login.html')
+
 
 
 
