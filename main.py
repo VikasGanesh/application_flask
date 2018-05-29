31ec56841f387de9f4ea6f54f4cf1eff098515ed
VikasGanesh <vikasyrg@gmail.com>
2018-05-29 21:59:59 +0530

    login post method

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 app=Flask(__name__)
 
 
 @app.route('/')
+def Home():
+    return render_template( 'login.html' )
+
+@app.route('/login', methods = ['POST'])
 def Login():
-    return render_template('login.html')
+    username=request.form['username']
+    password=request.form['password']
+    
+
+
+
 
 
 if __name__ == '__main__':
